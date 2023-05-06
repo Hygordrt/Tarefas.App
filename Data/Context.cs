eb158863155ebfb99e131527ab30a1dc3dcd1ecd
Hygor Divino Ribeiro Teles <divinohygor@gmail.com>
2023-05-06 18:07:18 -0300

    - Criação do meu context definindo o Id como chave primaria

---
+++ Data/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tarefas.App.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Task> Tarefas { get; set; }
+
+        public Context(DbContextOptions<Context> opcoes) : base(opcoes)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>()
+                .HasKey(t => t.Id)
+                .HasAnnotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
