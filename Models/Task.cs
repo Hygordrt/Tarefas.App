b9726beabc52df5fa7d7213831132b1b61fb7a0b
Hygor Divino Ribeiro Teles <divinohygor@gmail.com>
2023-05-06 10:47:56 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Task.cs
@@ -0,0 +1,12 @@
+﻿namespace Tarefas.App.Models
+{
+    public class Task
+    {
+        public int Id { get; set; } 
+
+        public string Description { get; set; }    
+
+        public bool IsCompleted { get; set; } 
+ 
+    }
+}
