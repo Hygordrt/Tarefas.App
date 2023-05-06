6471504dc8a070168967433d8173d3914b890623
Hygor Divino Ribeiro Teles <divinohygor@gmail.com>
2023-05-06 18:05:36 -0300

    -Criação do modelo.

---
+++ Models/TaskModels/Task.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Task
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public bool IsCompleted { get; set; } = false;
+}
