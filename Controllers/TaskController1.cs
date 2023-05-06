c177dd2745fa5e0ecb39a286d283e39c2639635e
Hygor Divino Ribeiro Teles <divinohygor@gmail.com>
2023-05-06 19:26:39 -0300

    - Operação para criar nova tarefa

--- Controllers/TaskController1.cs
+++ Controllers/TaskController1.cs
@@ -15,7 +15,27 @@ namespace Tarefas.App.Controllers
 
         public async Task<IActionResult> Index()
         {
+            // Operação de listagem
             return View(await _context.Tarefas.ToListAsync());
+        }
+
+        // Operação para criação de uma nova tarefa
+        [HttpGet]
+        public IActionResult NovaTarefa()
+        {
+            return View();
+        }
+
+        // Inserir dados no banco
+        [HttpPost]
+        public async Task<IActionResult> NovaTarefa(Task task)
+        {
+            // Passando a tarefa criada
+            await _context.Tarefas.AddAsync(task);
+            // Salvando
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
