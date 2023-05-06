a0a5d031a01f787dfaaca14d89bcdb83128ac6ef
Hygor Divino Ribeiro Teles <divinohygor@gmail.com>
2023-05-06 19:20:29 -0300

    - Configurando a controler

---
+++ Controllers/TaskController1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tarefas.App.Data;
+
+namespace Tarefas.App.Controllers
+{
+    public class TaskController1 : Controller
+    {
+        private readonly Context _context;
+
+        public TaskController1(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tarefas.ToListAsync());
+        }
+    }
+}
